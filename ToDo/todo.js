a290c7b5b2f9c9813f1b5daf4c4cddf4098a286c
Shreya S <116427410+04shreya-code@users.noreply.github.com>
2023-12-02 22:21:43 +0530

    Add files via upload

---
+++ ToDo/todo.js
@@ -0,0 +1,109 @@
+window.addEventListener('load', () => {
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoform = document.querySelector('#new-todo-form');
+
+    const username = localStorage.getItem('username') || '';
+    nameInput.value = username;
+
+    nameInput.addEventListener('change', e => {
+        localStorage.setItem('username', e.target.value);
+    });
+
+    newTodoform.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const todo = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.category.value,
+            done: false,
+            createdAt: new Date().getTime()
+        };
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+        e.target.reset();
+        displayTodos();
+    });
+    displayTodos();
+});
+
+function displayTodos() {
+    const todoList = document.querySelector('#todo-list');
+    todoList.innerHTML = '';
+
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+    todos.forEach((todo, index) => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = todo.done;
+        span.classList.add('bubble');
+
+        if (todo.category === 'personal') {
+            span.classList.add('personal');
+        } else {
+            span.classList.add('business');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteButton.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+        edit.innerHTML = 'Edit';
+        deleteButton.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteButton);
+        todoItem.appendChild(label);
+        todoItem.appendChild(content);
+        todoItem.appendChild(actions);
+        todoList.appendChild(todoItem);
+
+        if (todo.done) {
+            todoItem.classList.add('done');
+        }
+
+        input.addEventListener('click', e => {
+            todos[index].done = e.target.checked;
+            localStorage.setItem('todos', JSON.stringify(todos));
+            if (todos[index].done) {
+                todoItem.classList.add('done');
+            } else {
+                todoItem.classList.remove('done');
+            }
+        });
+
+        edit.addEventListener('click', () => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly', true);
+                todos[index].content = e.target.value;
+                localStorage.setItem('todos', JSON.stringify(todos));
+                displayTodos();
+            });
+        });
+
+        deleteButton.addEventListener('click', () => {
+            todos = todos.filter((_, idx) => idx !== index);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            displayTodos();
+        });
+    });
+}
